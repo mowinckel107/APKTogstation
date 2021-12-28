9ebfff50738b7d59c4ae5f1bc14c5b2631eed573
Your Name <you@example.com>
2021-12-28 22:02:12 +0100

    Added the functors in Controltower. Added functions to call those functors in different ways via boost::bind and update the signal objects the Trains have references too when those signals are called

--- controlTower.h
+++ controlTower.h
@@ -3,31 +3,79 @@
 
 
 #include <boost/signals2.hpp>
+#include <boost/bind/bind.hpp>
 #include "trainTrack.h"
 #include "train.h"
 #include <vector>
+#include <map>
 
-struct TrainCommunication
+class TrainSignalFunctor;
+
+struct TrainCommunicationAndRoute
 {
-boost::signals2::signal<void (std::string)> A;
-boost::signals2::signal<void (std::string)> B;
+    boost::signals2::signal<void ()> * leavingSignal;
+    boost::signals2::signal<bool ()> * isTrainTrackOccupiedSignal;
+    std::vector<unsigned int> route;
+};
+
+struct TrainTracker
+{
+    boost::signals2::signal<void ()> leavingSignal;
+    boost::signals2::signal<bool ()> isTrainTrackOccupiedSignal;
+    int howFarAlongRoute = 0;
+    std::vector<unsigned int> route;
 
 };
+
 
 class ControlTower
 {
     public:
-        static TrainCommunication * GetRouteAndSignals(unsigned int startingTrainTrackID);
         ControlTower();
-    private:
-        std::vector<TrainCommunication> trainSignals; // These should be saved in a container of type std::set
+        TrainCommunicationAndRoute GetRouteAndSignals(unsigned int startingTrainTrackID, int TrainID);
+        void UpdateTrainCommunicationLeave(int trainID);
+        void UpdateTrainCommunicationEnter(int trainID);
         
+    private:
+        std::vector<std::vector<unsigned int>> trainRouteForTrainOutput0;
         std::vector<std::vector<unsigned int>> trainRouteForTrainOutput1;
         std::vector<std::vector<unsigned int>> trainRouteForTrainOutput2;
-        std::vector<std::vector<unsigned int>> trainRouteForTrainOutput3;
 
-        std::vector<TrainTrack*> trainTracks;
+        std::map<int, TrainTracker> trainTrackers;
+        std::map<int, TrainSignalFunctor> trainSignalFunctors;
 
+};
+
+
+
+
+class TrainSignalFunctor
+{
+private:
+    bool isContainingTrain = false;
+public:
+    
+
+
+    bool operator()(bool isEnteringRequest, int trainID)
+    {
+        // if it is a entering request and there is already a train on the TrainTrack
+        if(isEnteringRequest && isContainingTrain)
+        {
+            return false; // Train may not enter
+        }
+        // if it is a entering request and the TrainTrack is free
+        else if (isEnteringRequest)
+        {
+            return true; // Train may enter
+        }
+        // if it is a "leaving"
+        else
+        {
+            isContainingTrain = false;
+            return true;
+        }
+    }
 };
 
 
