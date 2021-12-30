155e96b217ba8538cf86f2bcc248085f4580c3fc
Your Name <you@example.com>
2021-12-30 18:02:52 +0100

    Now compiles and works. Working on SFINAE implementation

--- container.h
+++ container.h
@@ -7,7 +7,7 @@ template <Temperature t, State s>
 class Container
 {
 private:
-    Cargo<t, s>* cargo_ptr;    // Void pointer to later point to cargo when cargo is created
+    Cargo<t,s>* cargo_ptr;    // Void pointer to later point to cargo when cargo is created
 public:
     Container();                    // Creates container and empty cargo
     Container(std::string cargo);   // Creates container with cargo
@@ -21,39 +21,45 @@ public:
 // If not, compiler has no idea how to implement functions
 
 template <Temperature t, State s>
-Container<t, s>::Container() : cargo_ptr(&Cargo<t,s>("Empty"))
+Container<t, s>::Container() : cargo_ptr(nullptr)
 {
     
 }
 
 template <Temperature t, State s>
-Container<t, s>::Container(std::string cargo) : cargo_ptr(&Cargo<t, s>(cargo))
+Container<t, s>::Container(std::string cargo) : cargo_ptr(new Cargo<t, s>(cargo))
 {
-    
+
 }
 
 template <Temperature t, State s>
 Container<t, s>::~Container() 
 {
-    cargo_ptr->~Cargo();
+    if (cargo_ptr != nullptr)
+        delete cargo_ptr;
 }
 
 template <Temperature t, State s>
 std::string Container<t, s>::seeCargo()
 {
-    return cargo_ptr->getCargo();
+    if (cargo_ptr != nullptr)
+        return cargo_ptr->getCargo();
+    else
+        return "Empty";
 }
 
 template <Temperature t, State s>
 void Container<t, s>::addCargo(std::string cargo)
 {
-
+    if (cargo_ptr == nullptr)
+        cargo_ptr = new Cargo<t, s>(cargo);
 }
 
 template <Temperature t, State s>
 void Container<t, s>::removeCargo()
 {
-    
+    if (cargo_ptr != nullptr)
+        cargo_ptr = nullptr;
 }
 
 #endif // __CONTAINER_H__
