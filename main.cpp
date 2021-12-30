fe6c44a2ff1eb56298ea53298348a02f47b9d3f2
Vymarus <Carstenkartze@gmail.com>
2021-12-30 14:44:17 +0100

    Added testing. Have to switch to linux to test

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include "trainTrack.h"
 #include "train.h"
 #include "controlTower.h"
+#include "container.h"
 
 #include <vector>
+#include <iostream>
 
 #include "tests.h"
 
 
 int main()
 {
-    RunTests(); // Uncomment/Comment to run/not run the tests
+    //RunTests(); // Uncomment/Comment to run/not run the tests
+    Container<cold, liquid> water("water");
 
-
+    std::cout << water.seeCargo() << std::endl;
 
     return 0;
 }
